b11f0fe620670c7734c11fc4329422debadbd4c7
laofengs <laofengs@gmail.com>
2021-08-08 14:10:39 -0400

    commit the mofified in ch1 branch

--- src/parse.sh
+++ src/parse.sh
@@ -1,2 +1,3 @@
 first file ==> parse.sh
 this is in new branch develop_poc
+I am in new branch develop_poc_ch1
--- src/transform.sh
+++ src/transform.sh
@@ -1,2 +1,3 @@
 second file ==> trasnform.sh
 this is in new branch develop_poc
+I am in new branch develop_poc_ch1
